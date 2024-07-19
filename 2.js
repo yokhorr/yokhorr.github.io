90e7e91bf2892b07c94f8c1953678d6dbd299441
yokhor <yokhor06@mail.ru>
2024-07-19 17:49:55 +1000

    Current project state added to git

---
+++ 2.js
@@ -0,0 +1,11 @@
+let jsonData;
+
+fetch('data/films_vladivostok_19_07_2024.json')
+  .then(response => response.json())
+  .then(data => {
+    jsonData = data;
+    console.log(jsonData); // Use jsonData here
+  })
+  .catch(error => console.error('Error:', error));
+
+console.log(jsonData)
