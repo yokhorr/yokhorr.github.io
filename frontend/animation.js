9c06a21476db07e69b4fb9e176a795d6e87b5206
Nikita M. <nikita.malyshev0007@gmail.com>
2024-07-21 13:20:08 +1000

    change cart style & started filing containers

--- frontend/animation.js
+++ frontend/animation.js
@@ -1,55 +1,84 @@
+//Фильтры____________________________________________________
 const dropdowns = document.querySelectorAll('.dropdown');
 
+// Итерируемся по каждому элементу .dropdown
 dropdowns.forEach(dropdown => {
+    // Получаем ссылки на элементы внутри .dropdown
     const select = dropdown.querySelector('.select');
     const caret = dropdown.querySelector('.caret');
     const menu = dropdown.querySelector('.menu');
     const options = dropdown.querySelectorAll('.menu li');
     const selected = dropdown.querySelector('.selected');
 
-
+    // Добавляем обработчик клика на .select
     select.addEventListener('click', () => {
+        // Переключаем классы для анимации
         select.classList.toggle('select-clicked');
         caret.classList.toggle('caret-rotate');
         menu.classList.toggle('menu-open');
     });
+
+    // Итерируемся по каждому элементу .menu li (варианту выбора)
     options.forEach(option => {
+        // Добавляем обработчик клика на каждый вариант выбора
         option.addEventListener('click', () => {
+            // Обновляем текст выбранного элемента
             selected.innerText = option.innerText;
+            // Снимаем классы для закрытия выпадающего списка
             select.classList.remove('select-clicked');
             caret.classList.remove('caret-rotate');
             menu.classList.remove('menu-open');
-            option.forEach(option => {
+
+            // Снимаем класс 'active' со всех вариантов выбора
+            options.forEach(option => {
                 option.classList.remove('active');
             });
 
+            // Добавляем класс 'active' к выбранному варианту
             option.classList.add('active');
         });
     });
 });
 
 
-
+//Логотип_____________________________________________________
+// Получаем первый заголовок h1 на странице
 const target = window.document.getElementsByTagName('h1')[0]
 
+// Функция для добавления анимации мерцания к букве
 const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+
+// Функция для добавления случайного цвета к букве
 const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+
+// Функция для создания мерцающего и разноцветного текста
 const flickerAndColorText = text => 
   text
+    // Разбиваем текст на отдельные буквы
     .split('')
+    // Добавляем анимацию мерцания к каждой букве
     .map(flickerLetter)
+    // Добавляем случайный цвет к каждой букве
     .map(colorLetter)
+    // Объединяем буквы обратно в строку
     .join('');
+
+// Функция для применения мерцающего и разноцветного текста к элементу
 const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
 
 
+// Применяем мерцающий и разноцветный текст к заголовку при загрузке страницы
 neonGlory(target);
+
+// Добавляем обработчик клика, чтобы при каждом клике по заголовку текст снова становился мерцающим и разноцветным
 target.onclick = ({ target }) =>  neonGlory(target);
 
+
+//Календарь___________________________________________________
 const datePicker = document.getElementById('date-picker');
 const today = new Date();
 const daysOfWeek = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const months = ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'];
 let lastHoveredItem = null; // Храним ссылку на последний элемент, над которым наведен курсор
 
 // Создаем массив дат
