4030264a9582c395ab5ac8a8ae209d31f09a3851
Nikita M. <nikita.malyshev0007@gmail.com>
2024-07-25 21:02:02 +1000

    banner 'Not Found'

--- frontend/animations.js
+++ frontend/animations.js
@@ -337,3 +337,51 @@ function scrollToTop() {
 
 const endButton = document.querySelector('.btn');
 endButton.addEventListener('click', scrollToTop);
+
+//Баннер_____________________________________________
+
+// Скрываем баннер по умолчанию
+document.getElementById("empty-result").classList.add("hidden");
+
+// Функция для показа баннера
+function showEmptyResult() {
+  document.getElementById("empty-result").classList.remove("hidden");
+}
+
+// Функция для скрытия баннера
+function hideEmptyResult() {
+  document.getElementById("empty-result").classList.add("hidden");
+}
+
+var otTitle = document.getElementById("textBox"),
+		sSpan = document.getElementsByClassName('text').length,
+		letters = document.getElementsByClassName('text'),
+		whichLetter = 0,
+		trailAmount = 7;
+
+
+//Used to get random colors
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+//Animate color change 
+setInterval(function() {
+	var rColor = getRandomColor();
+	var tColor = getRandomColor();
+	if (whichLetter < sSpan + trailAmount) {
+		if (whichLetter > (trailAmount - 1)) {
+			letters[(whichLetter - trailAmount)].style.color = 'whitesmoke';
+		}
+		if (whichLetter < sSpan) {
+			letters[whichLetter].style.color = rColor;
+			letters[whichLetter].style.textShadow = '0px 0px 10px ' + tColor;
+		}
+		whichLetter++;
+	} else if (whichLetter > sSpan + (trailAmount - 1)) whichLetter = 0;
+}, 75)
