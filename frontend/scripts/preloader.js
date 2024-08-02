ba2b890886633384310c4a9b3606092e39600f76
Nikita M. <nikita.malyshev0007@gmail.com>
2024-08-03 00:02:42 +1000

    bug fix

--- frontend/scripts/preloader.js
+++ frontend/scripts/preloader.js
@@ -1,33 +1,32 @@
 // Сбросить позицию прокрутки до начала страницы
-window.scrollTo(0, 0);
-
-// Заблокировать прокрутку до начала анимации прелоадера
-document.body.style.overflowY = "hidden"; 
+document.body.style.overflowY = "hidden";
 
 gsap.fromTo(
-  ".loading-page",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    display: "none",
-    duration: 1.1,
-    delay: 1,
-    onComplete: () => {
-      // Разрешить прокрутку после завершения анимации прелоадера
-      document.body.style.overflowY = "auto";
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0.9,
+      display: "none",
+      duration: 1,
+      onComplete: () => {
+        window.scrollTo(0, 0); // Сбросьте прокрутку после завершения анимации
+        // Разрешить прокрутку после завершения анимации прелоадера
+        document.body.style.overflowY = "auto";
+      },
+      delay: 1,
+    }
+  );
+  
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
     },
-  }
-);
-
-gsap.fromTo(
-  ".logo-name",
-  {
-    y: 50,
-    opacity: 0,
-  },
-  {
-    y: 0,
-    opacity: 1,
-    duration: 1
-  }
-);
+    {
+      y: 0,
+      opacity: 0.5,
+      duration: 0.9,
+      delay: 0,
+    }
+  );
