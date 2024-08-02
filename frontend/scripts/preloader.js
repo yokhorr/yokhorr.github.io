f0c8b21ab09fa24da4d0672e4db04b43750f90f3
Nikita M. <nikita.malyshev0007@gmail.com>
2024-08-02 20:40:07 +1000

    bug fix

--- frontend/scripts/preloader.js
+++ frontend/scripts/preloader.js
@@ -1,24 +1,31 @@
 gsap.fromTo(
-    ".loading-page",
-    { opacity: 1 },
-    {
-      opacity: 0,
-      display: "none",
-      duration: 1.5,
-      delay: 2,
-    }
-  );
-  
-  gsap.fromTo(
-    ".logo-name",
-    {
-      y: 50,
-      opacity: 0,
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 2,
+    onComplete: () => {
+      // Разрешить прокрутку после завершения анимации прелоадера
+      document.body.style.overflowY = "auto";
     },
-    {
-      y: 0,
-      opacity: 1,
-      duration: 2,
-      delay: 0.5,
-    }
-  );
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
+
+// Заблокировать прокрутку до начала анимации прелоадера
+document.body.style.overflowY = "hidden"; 
