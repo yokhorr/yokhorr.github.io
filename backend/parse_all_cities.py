016364d962b750afb6f53cf60b0ee2c8ce8d9750
yokhor <yokhor06@mail.ru>
2024-08-13 00:31:23 +1000

    ✨ feat(parser): Add time log
    Add start and end time of parsing process

--- backend/parse_all_cities.py
+++ backend/parse_all_cities.py
@@ -4,6 +4,8 @@ import fetch_data
 
 def main() -> None:
     start_time = time.time()
+    start_time_str = time.strftime("%H:%M:%S", time.localtime(start_time))
+    print(f'Parsing all cities started at {start_time_str}', end='\n\n')
 
     cities = [
         'vladivostok', 'artem', 'arsenyev', 'ussuriysk', 'nakhodka', 'spassk', 'vrangel', 'dalnegorsk',
@@ -14,7 +16,8 @@ def main() -> None:
         fetch_data.fetch_data(city)
 
     end_time = time.time()
-    print(f'Parsing all cities finished in {int(end_time - start_time)} seconds')
+    end_time_str = time.strftime("%H:%M:%S", time.localtime(end_time))
+    print(f'Parsing all cities finished at {end_time_str} in {int(end_time - start_time)} seconds')
 
 
 if __name__ == '__main__':
