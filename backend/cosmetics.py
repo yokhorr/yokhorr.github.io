35b664a2945981e127408bb7ee8110bdb1ac0bd9
yokhor <yokhor06@mail.ru>
2024-08-15 22:57:44 +1000

    📃 docs(backend): Fix copyright notice

--- backend/cosmetics.py
+++ backend/cosmetics.py
@@ -1,3 +1,14 @@
+# © 2024 Solyanik Egor   <yokhor06@mail.ru>
+#
+# This code is part of Vkadre project, a service for finding film shows.
+# It is licensed under the AGPL-3.0,
+# you may not use this file except in compliance with the license.
+#
+# License text: https://www.gnu.org/licenses/agpl-3.0.en.html
+# About project: https://github.com/yokhorr/yokhorr.github.io
+# SPDX-License-Identifier: AGPL-3.0
+
+
 # 1 апреля -> 01.04
 def strict_date_format(text: str) -> str:
     text = text.split()
